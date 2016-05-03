bfc8457de608325e1b36c98a6433cbe4d7569229
sikB <sikhovski@icloud.com>
2016-05-03 13:56:53 -0400

    styled the register page

--- routes/index.js
+++ routes/index.js
@@ -17,15 +17,16 @@ router.get('/register', function(req, res,next){
 router.post('/registerProcessed', function(req, res,next){
 	if(req.body.password != req.body.password2){
 		res.redirect('/register?failure=password')
+	}else{
+		var newAccount = new Account({
+			username: req.body.username,
+			password: req.body.password,
+			emailAddress: req.body.emailAddress
+		});
+		newAccount.save();
+		res.json(req.body);
+		// res.render('register', {});
 	}
-	var newAccount = new Account({
-		username: req.body.username,
-		password: req.body.password,
-		emailAddress: req.body.emailAddress
-	});
-	newAccount.save();
-	res.json(req.body);
-	// res.render('register', {});
 })
 
 module.exports = router;

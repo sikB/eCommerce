4e44854bba8d5d0e775db663d2ef97d835c9768c
sikB <sikhovski@icloud.com>
2016-05-03 13:31:11 -0400

    new route
    added a register route
    registrations are stored on a db for the future long ins
    restyled the header to be on all pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var mongoUrl = 'mongodb://localhost:27017/coffee';
+var mongoose = require('mongoose');
+var Account = require('../model/accounts');
+mongoose.connect(mongoUrl);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Cigar' });
 });
+
+router.get('/register', function(req, res,next){
+	res.render('register', {failure: req.query.failure});
+})
+
+router.post('/registerProcessed', function(req, res,next){
+	if(req.body.password != req.body.password2){
+		res.redirect('/register?failure=password')
+	}
+	var newAccount = new Account({
+		username: req.body.username,
+		password: req.body.password,
+		emailAddress: req.body.emailAddress
+	});
+	newAccount.save();
+	res.json(req.body);
+	// res.render('register', {});
+})
 
 module.exports = router;

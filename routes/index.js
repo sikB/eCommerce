d09b34991c94ec0ae5f1968f113141b67bff4ce5
sikB <sikhovski@icloud.com>
2016-05-04 10:38:11 -0400

    log in updated
    log in page created
    log in user and password matching created to verify previously created
    accounts

--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,11 @@ mongoose.connect(mongoUrl);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Cigar' });
+	res.render('index', { title: 'Cigar', username: req.session.username, page: 'home' });
 });
 
 router.get('/register', function(req, res,next){
-	res.render('register', {failure: req.query.failure});
+	res.render('register', {page: 'register', failure: req.query.failure});
 })
 
 router.post('/registerProcessed', function(req, res,next){
@@ -23,7 +23,7 @@ router.post('/registerProcessed', function(req, res,next){
 	}else{
 		var newAccount = new Account({
 			username: req.body.username,
-			password: bcrypt.hashSynch(req.body.password),
+			password: bcrypt.hashSync(req.body.password),
 			emailAddress: req.body.emailAddress
 		});
 		newAccount.save();
@@ -32,8 +32,35 @@ router.post('/registerProcessed', function(req, res,next){
 	}
 })
 
+router.get('/login', function(req, res, next){
+	res.render('login', {page: 'login'});
+});
+
+router.post('/login', function(req, res, next){
+
+	Account.findOne(
+		{username: req.body.username},
+		function (err, doc){
+			//doc is the document returned from our Mongo query. It has a property for each field.
+			//We need to check the password in the db (doc.password) against the submitted password through bcrypt
+			var loginResult = bcrypt.compareSync(req.body.password, doc.password);
+			if(loginResult){
+				//Hashes matched. Set up req.session.username and move them on
+				req.session.username = req.body.username;
+				res.redirect('/options');
+			}else{
+				//Hashes did not match or doc not found. Set them back to login
+				res.redirect('/login?failure=password')
+			}
+	});
+});
+
 router.get('/order', function(req,res,next){
-	res.render('order', {username: req.session.username});
+	if(!req.session.username){
+		res.redirect('/register');
+	}else{
+		res.render('order', {username: req.session.username});
+	}
 });
 
 module.exports = router;
